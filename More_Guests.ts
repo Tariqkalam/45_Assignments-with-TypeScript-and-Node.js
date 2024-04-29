050e9582e10ad94ec5019e1bd26e026c5912f820
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:30:58 +0500

    Add files via upload

---
+++ More_Guests.ts
@@ -0,0 +1,27 @@
+
+let guestNames: string[] = ['Zia Khan', 'Daniyal Nagori', 'Ameen Alam', 'Mubashir'];
+
+// for(let i = 0; i<guestNames.length; i++)
+//  {
+//     console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+// }
+
+let absentGuest: string = guestNames[3];
+guestNames[3] = 'Anas'
+for(let i = 0; i<guestNames.length; i++)
+{
+    console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+}
+
+console.log(`Sir ${absentGuest}, is not available and can,t make it.`)
+
+console.log('Good News! We have a big table and inviting three more guests now\n');
+
+guestNames.unshift('Allama Bashire Farooqi');
+guestNames.splice(3, 0, 'Wajid Hussain');
+guestNames.push('Basit Hussain');
+
+for(let i = 0; i<guestNames.length; i++)
+{
+    console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+}
