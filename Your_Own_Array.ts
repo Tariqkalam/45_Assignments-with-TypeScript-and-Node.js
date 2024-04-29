514704598db32db69f94a61e2b9775b75868ac4e
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:29:10 +0500

    Add files via upload

---
+++ Your_Own_Array.ts
@@ -0,0 +1,7 @@
+let transportations: string[] = ['Motor Vehicle', 'Motorbike', 'Train', 'Aeroplan'];
+
+for (let i = 0; i< transportations.length; i++) {
+
+    console.log('I Would like to own a '+ transportations[i]);
+    
+}
