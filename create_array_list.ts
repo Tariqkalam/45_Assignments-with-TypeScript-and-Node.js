90edd01f01de492b7900279408747b4faca2f66f
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:33:54 +0500

    Add files via upload

---
+++ create_array_list.ts
@@ -0,0 +1,14 @@
+// Assignment 20
+
+// Think of something you could store in a array. For example, you could make a list of mountains, 
+// rivers, countries, cities, languages, or anything else you’d like.
+// Write a program that creates a list containing these items.
+
+let rivers : string[] = [];
+
+rivers.push('Indus');
+rivers.push('Chanab');
+rivers.push('Satlaj');
+rivers.push('Jehlum');
+
+console.log(rivers);
