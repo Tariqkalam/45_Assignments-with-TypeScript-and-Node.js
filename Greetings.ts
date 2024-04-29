a716802dd52e1a6113ac8594c6c45516f5cecf7e
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:27:54 +0500

    Add files via upload

---
+++ Greetings.ts
@@ -0,0 +1,8 @@
+
+let Names: string[] = ['Ayzal', 'Aariz', 'Ayesha', 'Alishba', 'Mysha'];
+
+for (let i = 0; i<Names.length; i++) {
+
+    console.log(`Hi, ${Names[i]}! You are very good student`);
+
+}
