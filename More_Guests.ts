8f7a17f972edd38c7c902858e93ad1620ea14c36
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:31:32 +0500

    Add files via upload

--- More_Guests.ts
+++ More_Guests.ts
@@ -8,15 +8,15 @@ let guestNames: string[] = ['Zia Khan', 'Daniyal Nagori', 'Ameen Alam', 'Mubashi
 
 let absentGuest: string = guestNames[3];
 guestNames[3] = 'Anas'
-for(let i = 0; i<guestNames.length; i++)
-{
-    console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
-}
+// for(let i = 0; i<guestNames.length; i++)
+// {
+//     console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+// }
 
 console.log(`Sir ${absentGuest}, is not available and can,t make it.`)
 
 console.log('Good News! We have a big table and inviting three more guests now\n');
-
+ 
 guestNames.unshift('Allama Bashire Farooqi');
 guestNames.splice(3, 0, 'Wajid Hussain');
 guestNames.push('Basit Hussain');
@@ -25,3 +25,16 @@ for(let i = 0; i<guestNames.length; i++)
 {
     console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
 }
+console.log('\nSorry right now we dont have big table, only two people will be invited');
+while(guestNames.length > 2){
+let removedGuest = guestNames.pop()
+console.log(`Sorry Sir, ${removedGuest}, currently you are not invited`);
+
+}
+for(let i = 0; i<guestNames.length; i++)
+{
+    console.log('Respected Sir, '+guestNames[i], '\n\nYou are still invited with us in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+}
+guestNames.splice(0, 3);
+console.log(guestNames);
+
