ec0344e1720c013b92ac3d83f22bf336c0dffba7
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:26:32 +0500

    Add files via upload

---
+++ Program_2.ts
@@ -0,0 +1,8 @@
+// My name is Tariq Kalam
+// Today Date is 09-March-2024
+
+
+// In this message described my Experiences
+
+console.log('Have extensive working experience in Data Analytics in many well known organizations');
+
