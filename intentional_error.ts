9ae292eeeec9e417e2d10ac8abd131698088a8e2
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:35:01 +0500

    Add files via upload

---
+++ intentional_error.ts
@@ -0,0 +1,14 @@
+// Assignment# 22
+
+// Intentional Error: If you haven’t received an array index error in one of your programs yet,
+// try to make one happen. Change an index in one of your programs to produce an index error.
+
+let arrayError : string[] = ['Asif', 'Tariq', 'Kashif', 'Sajjad'];
+
+// Accessing wrong index to create Error
+console.log(arrayError[4]);
+
+// Make sure you correct the error before closing the program.
+// Removing Error to call correct index
+
+console.log(arrayError[2]);
