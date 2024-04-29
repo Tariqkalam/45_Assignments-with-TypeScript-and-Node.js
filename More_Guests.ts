5c0f0897dd67074b5f0265875089dc6ab78b88d7
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:33:12 +0500

    Add files via upload

--- More_Guests.ts
+++ More_Guests.ts
@@ -13,9 +13,9 @@ guestNames[3] = 'Anas'
 //     console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
 // }
 
-console.log(`Sir ${absentGuest}, is not available and can,t make it.`)
+// console.log(`Sir ${absentGuest}, is not available and can,t make it.`)
 
-console.log('Good News! We have a big table and inviting three more guests now\n');
+// console.log('Good News! We have a big table and inviting three more guests now\n');
  
 guestNames.unshift('Allama Bashire Farooqi');
 guestNames.splice(3, 0, 'Wajid Hussain');
@@ -23,18 +23,26 @@ guestNames.push('Basit Hussain');
 
 for(let i = 0; i<guestNames.length; i++)
 {
-    console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+    // console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
 }
-console.log('\nSorry right now we dont have big table, only two people will be invited');
+// console.log('\nSorry right now we dont have big table, only two people will be invited');
 while(guestNames.length > 2){
 let removedGuest = guestNames.pop()
-console.log(`Sorry Sir, ${removedGuest}, currently you are not invited`);
+// console.log(`Sorry Sir, ${removedGuest}, currently you are not invited`);
 
 }
-for(let i = 0; i<guestNames.length; i++)
-{
-    console.log('Respected Sir, '+guestNames[i], '\n\nYou are still invited with us in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
-}
+// for(let i = 0; i<guestNames.length; i++)
+// {
+//     console.log('Respected Sir, '+guestNames[i], '\n\nYou are still invited with us in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+// }
 guestNames.splice(0, 3);
 console.log(guestNames);
 
+
+// Exercise 19
+
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+
+
+console.log(`Number of people you are inviting to dinner are : ${guestNames.length}`);
