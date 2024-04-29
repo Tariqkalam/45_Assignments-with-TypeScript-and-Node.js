e3d8ce7ccc886ea7f07744feb0956ef6bab9be38
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:25:36 +0500

    Add files via upload

---
+++ Program_1.ts
@@ -0,0 +1,8 @@
+// My name is Tariq Kalam
+// Today Date is 09-March-2024
+
+
+// In this message described my Qualifications
+
+console.log('B.Sc (Computer Sc.) & M.Sc. (Statistics) Also have certificates in many technologies');
+
