5a7f5dd8be9e136e17c941126c8e6bf138cdde07
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:49:56 +0500

    Add files via upload

---
+++ T_Shirt.ts
@@ -0,0 +1,14 @@
+// Assignment# 36. T-Shirt
+// Write a function called make_shirt() that accepts a size and the
+// text of a message that should be printed on the shirt. The function should print
+// a sentence summarizing the size of the shirt and the message printed on it.
+// Call the function.
+
+function make_shirt(size: string, text: string) {
+    console.log(`Creating a ${size} size of T-Shirt with the message: ${text}`);
+}
+
+make_shirt('Small', 'Tommy Hilfiger');
+make_shirt('Medium', 'Tommy Hilfiger');
+make_shirt('Large', 'Tommy Hilfiger');
+
