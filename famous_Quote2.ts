4891e30b8efba71f5c1c982cde3bf0d347213aca
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:20:05 +0500

    Add files via upload

---
+++ famous_Quote2.ts
@@ -0,0 +1,4 @@
+let famous_person:string = 'Quaid e Azam';
+let Message: string = ' "There are two powers in the world; one is the sword and the other is the pen." ';
+
+console.log(famous_person+' once said,'+ Message);
