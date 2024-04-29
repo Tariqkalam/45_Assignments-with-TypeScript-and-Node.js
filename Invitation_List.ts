ad16286e1d7ce1f58f3bb143ec3291c556572865
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-04-30 00:29:50 +0500

    Add files via upload

---
+++ Invitation_List.ts
@@ -0,0 +1,7 @@
+
+let guestNames: string[] = ['Zia Khan', 'Daniyal Nagori', 'Ameen Alam', 'Mubashir'];
+
+for(let i = 0; i<guestNames.length; i++) {
+
+    console.log('Respected Sir, '+guestNames[i], '\n\nI would like to invite you in a dinner, It would be honor for us\n\nWarm Regards\nThank You\n\nTariq Kalam')
+}
